94f6060eb7c90361d8a60dd0010c0057649b7edf
Darlena Winky <dwinky@ev-a1.kzn.21-school.ru>
2020-12-30 22:15:09 +0300

    v1.3

--- ft_print_d.c
+++ ft_print_d.c
@@ -6,7 +6,7 @@
 /*   By: dwinky <dwinky@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/29 21:45:39 by dwinky            #+#    #+#             */
-/*   Updated: 2020/12/30 21:05:22 by dwinky           ###   ########.fr       */
+/*   Updated: 2020/12/30 22:11:04 by dwinky           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int		len_of_num2(int nbr)
 {
 	int k;
 
+	if (nbr < 0) // переполнение
+		nbr = -nbr;
 	k = 1;
 	while (nbr > 9)
 	{
@@ -41,19 +43,29 @@ void	ft_print_d(t_unit *unit, int num)
 	int len_num;
 
 	len_num = len_of_num2(num);
-	if (unit)
+	if (unit->width == 0 && unit->precision == 0 && num == 0)
+		return ;
+	else if (unit->precision == 0 && num == 0)
+	{
+		ft_putchar_fd(' ', 1);
+		return ;
+	}
+	if (num < 0)
+		unit->width -= 1;
 	if (unit->flag == '-')
 	{
+		if (num < 0)
+			ft_putchar_fd('-', 1);
 		if (unit->precision)
 		{
 			put_counts_char('0', unit->precision - len_num);
-			put_nbr(num);
-			if (unit->width > unit->precision)
-				put_counts_char(' ', unit->width - len_num);
+			put_nbr(ft_abs(num));
+			if (unit->width > unit->precision) // ?????????
+				put_counts_char(' ', unit->width - unit->precision);
 		}
 		else
 		{
-			put_nbr(num);
+			put_nbr(ft_abs(num));
 			put_counts_char(' ', unit->width - len_num);
 		}
 	}
@@ -64,6 +76,8 @@ void	ft_print_d(t_unit *unit, int num)
 			if (unit->precision > len_num)
 			{
 				put_counts_char(' ', unit->width - unit->precision);
+				if (num < 0)
+					ft_putchar_fd('-', 1);
 				put_counts_char('0', unit->precision - len_num);
 			}
 			else
@@ -73,9 +87,13 @@ void	ft_print_d(t_unit *unit, int num)
 		}
 		else if (unit->precision > len_num)
 		{
+			if (num < 0)
+				ft_putchar_fd('-', 1);
 			put_counts_char('0', unit->precision - len_num);
 		}
-		put_nbr(num);
+		if (num < 0)
+				ft_putchar_fd('-', 1);
+		put_nbr(ft_abs(num));
 	}
 	else
 	{
@@ -84,17 +102,27 @@ void	ft_print_d(t_unit *unit, int num)
 			if (unit->precision > len_num)
 			{
 				put_counts_char(' ', unit->width - unit->precision);
+				if (num < 0)
+					ft_putchar_fd('-', 1);
 				put_counts_char('0', unit->precision - len_num);
 			}
 			else
 			{
 				put_counts_char(' ', unit->width - len_num);
+				if (num < 0)
+					ft_putchar_fd('-', 1);
 			}
 		}
 		else if (unit->precision > len_num)
 		{
+			if (num < 0)
+				ft_putchar_fd('-', 1);
 			put_counts_char('0', unit->precision - len_num);
 		}
-		put_nbr(num);
+		else if (num < 0)
+		{
+			ft_putchar_fd('-', 1);
+		}
+		put_nbr(ft_abs(num));
 	}
 }
